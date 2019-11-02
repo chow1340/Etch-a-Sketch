af2677a0bc976f62785ee33d61909c65cd2164be
chow1340 <chow1340@mylaurier.ca>
2019-11-02 17:54:01 -0400

    Almost done

---
+++ script.js
@@ -0,0 +1,64 @@
+
+const black = document.querySelector('.black');
+const eraser = document.querySelector('.eraser');
+const random = document.querySelector('.random');
+function Choice() {
+    if (black.checked) {
+       return 'black'
+    } else if (eraser.checked) {
+        return 'white'
+    } else if (random.checked){
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (var i=0; i<6; i++){
+            color += letters[Math.floor(Math.random()*16)];
+        }
+        return color; 
+    }
+}
+
+let numCols = 16
+
+
+function createColumns(numCols){
+    for (c=0; c<numCols; c++){
+        const column = document.createElement('div');
+        column.classList.add('column')
+        container.appendChild(column);
+        container.style.setProperty('--numCols', numCols) 
+        column.addEventListener('mouseover', function(){
+            column.style.backgroundColor= Choice()
+        })
+    }
+
+}
+let user = 16  
+let test = user - 1
+let numRows = test*numCols // number of rows created
+const container = document.querySelector('.container');
+
+function createRows(numRows){
+    for (r=0; r<numRows; r++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        container.style.setProperty('--numRows',test);
+        row.addEventListener('mouseover', function(){
+            row.style.backgroundColor = Choice()
+        })
+    }
+}
+
+createColumns(numCols);
+createRows(numRows);
+
+function resetAll(){
+    container.textContent = ''
+    createColumns(numCols);
+    createRows(numRows);
+} 
+
+
+
+document.getElementById('resetButton').addEventListener('click', resetAll())
+ 
