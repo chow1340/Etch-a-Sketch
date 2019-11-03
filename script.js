7ef140c1639913e3d3933d35636f3c70cd2188d2
chow1340 <chow1340@mylaurier.ca>
2019-11-03 11:35:48 -0500

    Final project

--- script.js
+++ script.js
@@ -20,20 +20,24 @@ function Choice() {
 let numCols = 16
 
 
+
 function createColumns(numCols){
     for (c=0; c<numCols; c++){
         const column = document.createElement('div');
         column.classList.add('column')
         container.appendChild(column);
-        container.style.setProperty('--numCols', numCols) 
+        /* container.style.setProperty('--numCols', numCols) */
         column.addEventListener('mouseover', function(){
             column.style.backgroundColor= Choice()
         })
     }
 
 }
-let user = 16  
-let test = user - 1
+
+let user = 16
+
+
+let test = user -1 ;
 let numRows = test*numCols // number of rows created
 const container = document.querySelector('.container');
 
@@ -42,23 +46,26 @@ function createRows(numRows){
         let row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        container.style.setProperty('--numRows',test);
+        /* container.style.setProperty('--numRows',test); */
         row.addEventListener('mouseover', function(){
             row.style.backgroundColor = Choice()
         })
     }
 }
 
-createColumns(numCols);
-createRows(numRows);
 
-function resetAll(){
+
+document.getElementById('resetButton').addEventListener('click', function(){
     container.textContent = ''
-    createColumns(numCols);
-    createRows(numRows);
-} 
-
-
-
-document.getElementById('resetButton').addEventListener('click', resetAll())
+    let numColsReset = document.getElementById('colNum').value
+    let numRowsReset = document.getElementById('rowNum').value
+    container.style.setProperty('--numCols', numColsReset);
+    
+    
+    let numRowsCalculation = numColsReset * numRowsReset;
+    container.style.setProperty('--numRows', numRowsReset);
+    createColumns(numColsReset);
+    createRows(numRowsCalculation);
+    
+})
  
